f0eb90cad02515c8952bb24de735e08d2c6dd107
Dean Cefola <Dean.Cefola@microsoft.com>
2019-02-14 21:53:56 -0500

    Disable-IPv6
    Script to disable IPv6 on Windows systems.
    will run commands and edit reg

---
+++ Disable IPv6.ps1
@@ -0,0 +1,27 @@
+﻿<#Author   : Dean Cefola
+# Creation Date: 01-08-2014
+# Usage      : Disable IPv6
+#
+#***********************************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 01-08-2014                       1.0       Intial Version
+#
+#************************************************************************************************
+#>
+
+
+#------------------------------Disable IPv6------------------------------#
+Netsh int teredo set state disabled
+Netsh int isatap set state disabled
+Netsh int 6to4 set state disabled undoonstop=disabled
+Write-Host -ForegroundColor Green "IPv6 Disabled on" 
+Write-Host -ForegroundColor Yellow -BackgroundColor Black $env:COMPUTERNAME
+
+
+#------------------------------Change Registry Settings------------------------------#
+Push-Location 
+Set-Location HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters 
+New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters -Name "DisabledComponents" -PropertyType "DWord" -Value 4294967295
+
+Pop-Location
