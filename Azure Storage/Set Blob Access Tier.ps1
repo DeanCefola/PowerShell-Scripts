cc2609a86478d7004389f1e75cdf5f18f24853ca
Dean Cefola <deacef@microsoft.com>
2019-03-12 10:58:55 -0400

    Create Set Blob Access Tier.ps1
    Add Script to change Blob Access Tier for Archive, Cool, Hot

---
+++ Azure Storage/Set Blob Access Tier.ps1
@@ -0,0 +1,24 @@
+﻿<#Author   : Dean Cefola
+# Creation Date: 1-12-2019
+# Usage      : Set Blob Storage Access Tier
+
+#**************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 1/12/2019                       1.0       Intial Version
+#
+#
+#***************************************************************************
+#>
+
+$RGName = 'CON-UE2-PD1-Shared-RG-01'
+$STName = 'msdean'
+$Container = 'sap'
+$StorageTier = "Cool"
+$Key = (Get-AzureRmStorageAccountKey -ResourceGroupName $RGName -Name $STName | select -First 1).Value
+$Context = New-AzureStorageContext -StorageAccountName $STName -StorageAccountKey $Key
+$Blob = Get-AzureStorageBlob -Container $Container -Context $Context
+
+$Blob.icloudblob.setstandardblobtier($StorageTier)
+
+
