d35137acd026b9cb7cb0265cfa07a131b8370fb5
Dean Cefola <deacef@microsoft.com>
2019-03-19 13:48:11 -0400

    file organization
    file organization

---
+++ Azure Storage/Prepare a Windows VHD or VHDX to upload to Azure.ps1
@@ -0,0 +1,105 @@
+﻿<#Author   : Dean Cefola
+# Creation Date: 10/16/2018
+# Usage      : Prepare a Windows VHD or VHDX to upload to Azure
+
+#**************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 10/16/2018                       1.0       Intial Version
+#
+#***************************************************************************
+#>
+
+
+##############################################
+#    Set Windows configurations for Azure    #
+##############################################
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\TimeZoneInformation' -name "RealTimeIsUniversal" -Value 1 -Type DWord -force
+Set-Service -Name w32time -StartupType Automatic
+powercfg /setactive SCHEME_MIN
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -name "TEMP" -Value "%SystemRoot%\TEMP" -Type ExpandString -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment' -name "TMP" -Value "%SystemRoot%\TEMP" -Type ExpandString -force
+
+
+####################################
+#    Check the Windows services    #
+####################################
+Set-Service -Name bfe -StartupType Automatic
+Set-Service -Name dnscache -StartupType Automatic
+Set-Service -Name MpsSvc -StartupType Automatic
+Set-Service -Name dhcp -StartupType Automatic
+Set-Service -Name IKEEXT -StartupType Automatic
+Set-Service -Name iphlpsvc -StartupType Automatic
+Set-Service -Name netlogon -StartupType Manual
+Set-Service -Name netman -StartupType Manual
+Set-Service -Name nsi -StartupType Automatic
+Set-Service -Name termService -StartupType Manual
+Set-Service -Name RemoteRegistry -StartupType Automatic
+
+
+#################################################
+#    Update Remote Desktop registry settings    #
+#################################################
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDenyTSConnections" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "PortNumber" -Value 3389 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "LanAdapter" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SecurityLayer" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "fAllowSecProtocolNegotiation" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SecurityLayer" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "fAllowSecProtocolNegotiation" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "KeepAliveEnable" -Value 1  -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "KeepAliveInterval" -Value 1  -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "KeepAliveTimeout" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services' -name "fDisableAutoReconnect" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "fInheritReconnectSame" -Value 1 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "fReconnectSame" -Value 0 -Type DWord -force
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\Winstations\RDP-Tcp' -name "MaxInstanceCount" -Value 4294967295 -Type DWord -force
+Remove-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "SSLCertificateSHA1Hash" -force
+
+
+##########################################
+#    Configure Windows Firewall rules    #
+##########################################
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
+Enable-PSRemoting -force
+Set-NetFirewallRule -DisplayName "Windows Remote Management (HTTP-In)" -Enabled True
+Set-NetFirewallRule -DisplayGroup "Remote Desktop" -Enabled True
+Set-NetFirewallRule -DisplayName "File and Printer Sharing (Echo Request - ICMPv4-In)" -Enabled True
+
+
+###############################################################
+#    Verify VM is healthy, secure, and accessible with RDP    #
+###############################################################
+Chkdsk /f
+cmd
+bcdedit /set {bootmgr} integrityservices enable
+bcdedit /set {default} device partition=C:
+bcdedit /set {default} integrityservices enable
+bcdedit /set {default} recoveryenabled Off
+bcdedit /set {default} osdevice partition=C:
+bcdedit /set {default} bootstatuspolicy IgnoreAllFailures
+#Enable Serial Console Feature
+bcdedit /set {bootmgr} displaybootmenu yes
+bcdedit /set {bootmgr} timeout 5
+bcdedit /set {bootmgr} bootems yes
+bcdedit /ems {current} ON
+bcdedit /emssettings EMSPORT:1 EMSBAUDRATE:115200
+exit
+# Setup the Guest OS to collect a kernel dump on an OS crash event
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -name CrashDumpEnabled -Type DWord -force -Value 2
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -name DumpFile -Type ExpandString -force -Value "%SystemRoot%\MEMORY.DMP"
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\CrashControl' -name NMICrashDump -Type DWord -force -Value 1
+#Setup the Guest OS to collect user mode dumps on a service crash event
+$key = 'HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting\LocalDumps'
+if ((Test-Path -Path $key) -eq $false) {(New-Item -Path 'HKLM:\SOFTWARE\Microsoft\Windows\Windows Error Reporting' -Name LocalDumps)}
+New-ItemProperty -Path $key -name DumpFolder -Type ExpandString -force -Value "c:\CrashDumps"
+New-ItemProperty -Path $key -name CrashCount -Type DWord -force -Value 10
+New-ItemProperty -Path $key -name DumpType -Type DWord -force -Value 2
+Set-Service -Name WerSvc -StartupType Manual
+winmgmt /verifyrepository
+netstat -anob
+
+
