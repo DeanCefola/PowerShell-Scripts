80114f903f2244269e4ca36164e5c6b8478dbfb9
Dean Cefola <Dean.Cefola@microsoft.com>
2020-12-18 18:37:51 -0500

    Create Certificate_Self Signed.ps1
    CodeSigning Cert

---
+++ Certificate_Self Signed.ps1
@@ -0,0 +1,21 @@
+﻿<#Author       : Dean Cefola
+# Creation Date: 11-19-2019
+# Usage        : Self-Signed Certificates
+#
+#********************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 11/19/2019                     1.0        Intial Version
+#
+
+#*********************************************************************************
+#
+#>
+
+$cert = New-SelfSignedCertificate -CertStoreLocation Cert:\LocalMachine\My -DnsName <"ENTER-DNS-NAME"> -Type CodeSigningCert -Subject <"ENTER-SUBJECT-NAME"> -Verbose
+$cert
+$secPassword = ConvertTo-SecureString -String '<TYPE-PASSWORD>' -Force -AsPlainText
+$certPath = "Cert:\LocalMachine\My\$($Cert.Thumbprint)"
+Export-PfxCertificate -Cert $certPath -FilePath 'C:\temp\MSAzureAcademy-CodeSigning.pfx' -Password $secPassword
+
+# Import-PfxCertificate -Password $secPassword -FilePath 'C:\temp\WVD\MSIX\Code Signing Cert\MSAzureAcademy CodeSigning.pfx' -CertStoreLocation 'Cert:\LocalMachine\TrustedPublisher'
