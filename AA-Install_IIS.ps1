e96ccd6c87311f9742e266daa42dd5328024e274
Dean Cefola <Dean.Cefola@microsoft.com>
2021-01-18 16:31:41 -0500

    Update AA-Install_IIS.ps1
    add standard header back in

--- AA-Install_IIS.ps1
+++ AA-Install_IIS.ps1
@@ -1,3 +1,14 @@
+<#Author       : Dean Cefola	
+# Creation Date: 11-30-2017	
+# Usage        : Install IIS to Windows Server	
+#********************************************************************************	
+# Date                         Version      Changes	
+#------------------------------------------------------------------------	
+# 11/30/2017                     1.0        Intial Version	
+#	
+#*********************************************************************************	
+#	
+#>	
 install-windowsfeature -name Web-Server -IncludeManagementTools
 Set-Location -Path c:\inetpub\wwwroot
 Add-Content iisstart.htm "<H1><center>WELCOME to my Web Server $env:COMPUTERNAME, Azure Academy Rocks!</center></H1>"
