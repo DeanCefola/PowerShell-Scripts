e9621d4dad3ece7a610a3e3ed39526aa415bc744
Dean Cefola <Dean.Cefola@microsoft.com>
2022-05-11 13:03:11 -0400

    Update RouteTable.ps1
    add additional example lines

--- RouteTable.ps1
+++ RouteTable.ps1
@@ -1,6 +1,7 @@
 
 $UDRTable = @(
-    @{AddressPrefix='10.10.0.0/16';RTName="rt-10.12.108.0-cus-prod-001";RouteName='rt-10.10.0.0-16';RGName='rg-hub-prod-cus-001';Subnet='10.12.108.0/24';Hub='10.10.0.0/16';NextHopType='VirtualAppliance';NextHop='10.10.201.4'}    
+    @{AddressPrefix='10.10.0.0/16';RTName="rt-10.12.108.0-cus-prod-001";RouteName='rt-10.10.0.0-16';RGName='rg-hub-prod-cus-001';Subnet='10.12.108.0/24';Hub='10.10.0.0/16';NextHopType='VirtualAppliance';NextHop='10.10.201.4'}
+    @{AddressPrefix='10.11.0.0/16';RTName="rt-10.12.109.0-cus-prod-001";RouteName='rt-10.11.0.0-16';RGName='rg-hub-prod-cus-002';Subnet='10.12.109.0/24';Hub='10.10.0.0/16';NextHopType='VirtualAppliance';NextHop='10.10.201.4'}
 )
 ForEach($Job in $UDRTable) {
     Get-AzRouteTable `
@@ -17,6 +18,7 @@ ForEach($Job in $UDRTable) {
 
 $UDRTable = @(    
     @{AddressPrefix='10.10.0.0/16';RTName="rt-10.12.108.0-cus-prod-001";RouteName='rt-subnetspecific';RGName='rg-hub-prod-cus-001';Subnet='10.12.108.0/24';Hub='10.10.0.0/16';NextHopType='VirtualNetwork';NextHop=''}
+    @{AddressPrefix='10.11.0.0/16';RTName="rt-10.12.109.0-cus-prod-001";RouteName='rt-subnetspecific';RGName='rg-hub-prod-cus-002';Subnet='10.12.109.0/24';Hub='10.10.0.0/16';NextHopType='VirtualNetwork';NextHop=''}
 )
 ForEach($Job in $UDRTable) {
     Get-AzRouteTable `
