437b0a2dc33398be5c821109e31b4f67a81091b7
Dean Cefola <Dean.Cefola@microsoft.com>
2022-08-03 20:14:56 -0400

    Move Script
    Move to Storage Folder

---
+++ Storage/_Azure-Files-Handles.ps1
@@ -0,0 +1,60 @@
+﻿<#Author   : Dean Cefola
+# Creation Date: 04-08-2020
+# Usage      : AZURE - File Storage Handles
+
+#**************************************************************************
+# Date                         Version      Changes
+#------------------------------------------------------------------------
+# 04/08/2020                       1.0       Intial Version
+#
+#***************************************************************************
+#
+#>
+
+
+
+######################
+#    Set Variables   #
+######################
+$rgname = 'ADAuth-FSLogix-WVD'
+$StorageAccountName = 'adauthfslogixwvd000'
+$shareName = 'fslogix'
+
+
+##################################
+#    Set Storage Environment     #
+##################################
+$stname = Get-AzStorageAccount `
+    -ResourceGroupName $rgname `
+    -Name $StorageAccountName 
+$key = Get-AzStorageAccountKey `
+    -ResourceGroupName $rgname `
+    -Name $stname.StorageAccountName | select -First 1
+$storageContext = New-AzStorageContext `
+    -StorageAccountName $stname.StorageAccountName `
+    -StorageAccountKey $key.value
+$share = Get-AzStorageShare -Name $shareName `
+-Context $storageContext
+
+
+#####################################
+#    Check for open file handles    #
+#####################################
+Get-AzStorageFileHandle `
+    -Context $storageContext `
+    -ShareName $shareName `
+    -Recursive `
+    | Format-Table -AutoSize
+
+
+#################################
+#    Close open file handles    #
+#################################
+Close-AzStorageFileHandle `
+    -Context $storageContext `
+    -ShareName "fslogix" `
+    -Recursive `
+    -CloseAll `
+    -Verbose
+
+       
